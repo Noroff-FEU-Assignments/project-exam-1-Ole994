8d63e4fbbc1409e2b65400e4b7956d7bd462ea67
Ole994 <ole.korvald1994@gmail.com>
2021-05-24 11:14:33 +0200

    updated

--- js/contactPage.js
+++ js/contactPage.js
@@ -1,17 +1,48 @@
-// const form = document.querySelector (".contactForm")
-// const form = document.querySelector (".fName")
-// const form = document.querySelector (".nameError")
+function validation (){
+    const yourMame = document.querySelector(".yourName").value;
+    const subject = document.querySelector(".subject").value;
+    const phone = document.querySelector(".phone").value;
+    const email = document.querySelector(".email").value;
+    const message = document.querySelector(".message").value;
+    const error_message = document.querySelector(".error_message").value;
+    const text;
+
+    error_message.style.padding = "10px";
+
+    if(yourName.length <5){
+        text = "please enter your real name";
+        error_message.innerHtml = text;
+        return false;
+    }
+
+    if(subject.length <10){
+        text = "please enter correct subject";
+        error_message.innerHtml = text;
+        return false;
+    }
+
+    if(isNaN (phone) || phone.length != 10){
+        text = "please enter valid number";
+        error_message.innerHtml = text;
+        return false;
+    }
+
+    if(email.indexOf ("@") ==-1  || email.length <6){
+        text = "please enter valid email";
+        error_message.innerHtml = text;
+        return false;
+    }
+
+    if (message.length <= 166){
+        text = "please enter more text";
+        error_message.innerHtml = text;
+        return false;  
+    }
+
+
+alert ("Form submitted sucsessfully")
+return true;
 
 
 
-// function validateForm (event){
-//     event.preventDefault ();
-// }
-
-// if (checkLength(fName.value,0)= true){
-//     fName-error.style.display == "none";
-// } 
-
-// else{
-//     fName-error.style.display == "block";
-// }
+}
