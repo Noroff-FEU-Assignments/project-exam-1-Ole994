7aca0151ecdccdaaf121c2911deef87565f32378
Ole994 <69842859+Ole994@users.noreply.github.com>
2021-05-25 16:32:22 +0200

    postPage-edit

--- js/post.js
+++ js/post.js
@@ -1,7 +1,7 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-const spinner = document.querySelector(".lds-spinner"); 
+const spinner = document.querySelector(".lds-spinner");
 const url = `https://olekorvald.no/wp-json/wp/v2/posts/${id}?_embed`
 const postContent = document.querySelector(".post-content-post-page")
 const loading = document.querySelector(".loading");
@@ -11,8 +11,8 @@ fetch(url, {
   "method": "GET"
 })
   .then(response => response.json())
-  .then(data => renderCarousel(data) )
-  .finally(()=> loading.style.display = "none");
+  .then(data => renderCarousel(data))
+  .finally(() => loading.style.display = "none");
 
 const renderCarousel = (post) => {
 
@@ -20,10 +20,18 @@ const renderCarousel = (post) => {
   let imageUrl = post._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url;
   let htmlString = `
       
-              <div class"image-card-postsPage>
-              <div class="post-title"><h2>${post.title.rendered}</h2></div>
-              <div class="post-image"> <a class="navbar-links" href="post.html?id=${post.id}"><img class= "img-cards-single-post"src = "${imageUrl}"/></a></div>
-              <div class="post-text"> ${post.excerpt.rendered}</div>
+              <div class"image-card-postPage">
+                  <div class="post-title"><h2>${post.title.rendered}</h2></div>
+              
+                <div class="post-image">
+                  <a class="navbar-links" href="post.html?id=${post.id}">
+                  
+                  <img class= "img-cards-single-post-post-page"src = "${imageUrl}"/>
+                  </a>
+                </div>
+                <div class="post-text-post-page"> 
+                  ${post.excerpt.rendered}
+                </div>
               </div>
             
             
