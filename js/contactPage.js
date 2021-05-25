66c457859ef4fdd42cca177e9b182794f6c0c551
Ole994 <69842859+Ole994@users.noreply.github.com>
2021-05-25 19:10:26 +0200

    25.05.end.day

--- js/contactPage.js
+++ js/contactPage.js
@@ -1,48 +1,42 @@
-const yourName = document.querySelector("your_name")
+const form = document.querySelector("form#signup");
+// form.addEventListener("submit", validateForm
+// );
+const name = document.querySelector("name")
 const email = document.querySelector("email")
 const subject = document.querySelector("subject")
 const message = document.querySelector("message")
 const button = document.querySelector("button")
+// /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]
+// +)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.
+// [0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
 
     
 
 
-function checkIfButtonIsDisabled() {
-    // if all inputs pass validation enable the button
-    if (checkLength(yourName.value, 1) && checkLength(yourName.value, 4) && validateEmail(email.value)) {
-        button.disabled = false;
-    } else {
-        // clear the message
-        message.innerHTML = "";
-        // disable button
-        button.disabled = true;
-    }
-}
 
-// call the same function for each input's keyup event
-firstName.addEventListener("keyup", checkIfButtonIsDisabled);
-lastName.addEventListener("keyup", checkIfButtonIsDisabled);
-email.addEventListener("keyup", checkIfButtonIsDisabled);
 
-// function to run when the form is submitted
-function submitForm(event) {
-    event.preventDefault();
-    // display a message once the form has been submitted
-    message.innerHTML = '<div class="message">Your message has been sent</div>';
-    // clear all input values
-    form.reset();
-}
 
-form.addEventListener("submit", submitForm);
 
-// function to check if the length of the input value is valid
-function checkLength(value, len) {
-    if (value.trim().length >= len) {
-        return true;
-    } else {
-        return false;
-    }
-}
+
+
+
+
+
+
+
+
+/*Home*/
+
+
+
+
+
+
+
+
+
+
 
 function validateEmail(email) {
     const regEx = /\S+@\S+\.\S+/;
