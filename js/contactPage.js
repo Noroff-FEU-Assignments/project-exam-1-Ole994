5cf42b5e4879878f98591a136b1689060282da9f
Ole994 <69842859+Ole994@users.noreply.github.com>
2021-05-27 17:32:32 +0200

    nav

--- js/contactPage.js
+++ js/contactPage.js
@@ -1,70 +1,92 @@
-const form = document.querySelector("form#signup");
-//  form.addEventListener("submit", validateForm
-//  );
-const name = document.querySelector("#name")
-const nameError = document.querySelector("#name-error")
-const email = document.querySelector("#email")
-const subject = document.querySelector("#subject")
-const message = document.querySelector("#message")
-const button = document.querySelector("#submitFormButton")
-
-// /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]
-// +)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.
-// [0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const form = document.querySelector("#signup");
+form.addEventListener("submit", validateForm
+);
 
 
- 
-   
-function checkIfButtonIsDisabled() {
-    console.log(name.value,subject.value,email.value)
-    // if all inputs pass validation enable the button
-    if (checkLength(name.value, 3) && checkLength(subject.value, 4) && validateEmail(email.value)) {
-        button.disabled = false;
+const name = document.querySelector("#name");
+const nameError = document.querySelector("#name-error");
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#email-error");
+
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subject-error");
+
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#message-error");
+console.log(message)
+
+const button = document.querySelector("#submitFormButton");
+
+function validateForm(e) {
+    e.preventDefault();
+
+
+
+    // Validate Name, minimum 2 characters long, no digits
+    let submitName = name.value.trim();
+    nameError.innerHTML = "";
+
+
+
+    if (/\d/.test(submitName)) {
+        nameError.innerHTML += "The name cannot contain any digits!";
+    }
+    if (submitName < 5) {
+        nameError.innerHTML = "Dette går ikkke"
+    }
+
+
+
+
+
+
+
+    
+    // Validate e-mail
+    let submittedEmail = email.value.trim();
+    emailError.innerHTML = ""; // Clear earlier messages
+    let emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    //email pattern
+
+
+    if (!emailPattern.test(submittedEmail)) {
+        emailMsg.innerHTML += "Please enter a valid email";
+    }
+
+    let subjectPattern = subject.value.trim()
+    subjectError.innerHTML = "";
+    if (subjectPattern.length < 10) {
+        subjectError.innerHTML += "Please enter a valid em";
+    }
+
+    let messagePattern = message.value.trim()
+    messageError.innerHTML = "";
+    if (messagePattern.length < 25) {
+        messageError.innerHTML += "Please enter at least 15 digits";
+    }
+
+
+    if (nameMsg.innerHTML === "" && emailMsg.innerHTML === "" && subjectError.innerHTML === "" && messageError.innerHTML === "") {
+        console.log("Form submitted");
+        //form.submit();
     } else {
-        // clear the message
-        message.innerHTML = "";
-        // disable button
-        button.disabled = true;
+        console.log("Still validation errors");
     }
+
 }
 
-// call the same function for each input's keyup event
-name.addEventListener("keyup", checkIfButtonIsDisabled);
-email.addEventListener("keyup", checkIfButtonIsDisabled);
-subject.addEventListener("keyup", checkIfButtonIsDisabled);
 
-// function to run when the form is submitted
-function submitForm(event) {
-    event.preventDefault();
-    if (checkLength(name.value, 3)){
-        nameError.style.display="none"
-        
-    }
-    else{
-        nameError.style.display="block"
-    }
-    // display a message once the form has been submitted
-    message.innerHTML = '<div class="message">Your message has been sent</div>';
-    // clear all input values
-    form.reset();
-}
 
-form.addEventListener("submit", submitForm);
 
-// function to check if the length of the input value is valid
-function checkLength(value, len) {
-    if (value.trim().length >= len) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// let submitEmail
 
-function validateEmail(email) {
-    const regEx = /\S+@\S+\.\S+/;
-    const patternMatches = regEx.test(email);
-    return patternMatches;
-} 
+// if (!emailPattern.test(submitEmail)) {
+//     emailMsg.innerHTML += "Please enter a valid email";}
+
+
+// let emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
 
@@ -79,7 +101,6 @@ function validateEmail(email) {
 
 
 
-/*Home*/
 
 
 
@@ -91,8 +112,127 @@ function validateEmail(email) {
 
 
 
-function validateEmail(email) {
-    const regEx = /\S+@\S+\.\S+/;
-    const patternMatches = regEx.test(email);
-    return patternMatches;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function checkIfButtonIsDisabled() {
+//     console.log(name.value,subject.value,email.value)
+//     // if all inputs pass validation enable the button
+//     if (checkLength(name.value, 5) && checkLength(subject.value, 15) && validateEmail(email.value)) {
+//         button.disabled = false;
+//     } else {
+//         // clear the message
+//         message.innerHTML = "";
+//         // disable button
+//         button.disabled = true;
+//     }
+// }
+
+// // call the same function for each input's keyup event
+// name.addEventListener("keyup", checkIfButtonIsDisabled);
+// email.addEventListener("keyup", checkIfButtonIsDisabled);
+// subject.addEventListener("keyup", checkIfButtonIsDisabled);
+
+// // function to run when the form is submitted
+// function submitForm(event) {
+//     event.preventDefault();
+//     if (checkLength(name.value, 3)){
+//         nameError.style.display="none"
+
+//     }
+//     else{
+//         nameError.style.display="block"
+//     }
+//     // display a message once the form has been submitted
+//     message.innerHTML = '<div class="message">Your message has been sent</div>';
+//     // clear all input values
+//     form.reset();
+// }
+
+// form.addEventListener("submit", submitForm);
+
+// // function to check if the length of the input value is valid
+// function checkLength(value, len) {
+//     if (value.trim().length >= len) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// function validateEmail(email) {
+//     const regEx = /\S+@\S+\.\S+/;
+//     const patternMatches = regEx.test(email);
+//     return patternMatches;
+// } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*Home*/
+
+
+
+
+
+
+
+
+
+
+
+// function validateEmail(email) {
+//     const regEx = /\S+@\S+\.\S+/;
+//     const patternMatches = regEx.test(email);
+//     return patternMatches;
+// }
